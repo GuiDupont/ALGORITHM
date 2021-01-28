2ba33a130e88a5aadc61cf6b7cd853f8476469de
Guillaume DUPONT <gdupont@student.42.fr>
2021-01-28 17:29:50 +0100

    wip: prob2sum

---
+++ projects/prob2sum/includes/prob2sum.h
@@ -0,0 +1,9 @@
+#ifndef PROB2SUM_H
+# define PROB2SUM_H
+
+#include "../../../libft/includes/libft.h"
+
+t_list	**malloc_hashtable(int size);
+void	add_elem_to_table(t_list **table, long long nb);
+void	ft_swap_ll(long long *a, long long *b);
+#endif
